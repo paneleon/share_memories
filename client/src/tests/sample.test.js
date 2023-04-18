f3c8b9f44a0fff3c1cab16ff7dee3e0362d4c701
Eleonora <paneleon06@gmail.com>
2023-04-18 15:57:06 -0400

    Add tests to the project

---
+++ client/src/tests/sample.test.js
@@ -0,0 +1,7 @@
+function welcome(name) {
+    return `Welcome to memories, ${name}!`;
+}
+
+test('displays proper message', () => {
+    expect(welcome('Anne')).toBe('Welcome to memories, Anne!');
+});
