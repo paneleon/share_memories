efeae8fb1ca1484799d796a1cf306d097e8adfec
Eleonora <paneleon06@gmail.com>
2023-04-18 21:05:57 -0400

    Fix lint errors

--- client/src/tests/sample.test.js
+++ client/src/tests/sample.test.js
@@ -1,7 +1,7 @@
 function welcome(name) {
-    return `Welcome to memories, ${name}!`;
+  return `Welcome to memories, ${name}!`;
 }
 
 test('displays proper message', () => {
-    expect(welcome('Anne')).toBe('Welcome to memories, Anne!');
+  expect(welcome('Anne')).toBe('Welcome to memories, Anne!');
 });
