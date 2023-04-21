b6d278cf369562a20a1579cb8bad3d9d36a8b599
Thedyson <90479286+ThedysonLuzon@users.noreply.github.com>
2023-04-20 22:44:37 -0400

    Currently no posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -10,7 +10,7 @@ const Posts = ({ setCurrentId }) => {
 
   return (
     // #2 bug fixed removed <CircularProgress />
-    !posts.length ? <h1>No posts</h1> : (
+    !posts.length ? <h1>Currently no posts</h1> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6} md={6}>
